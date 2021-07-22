c96bf3b72691d0a44b5aa3dacdfe40b43be7c89b
thedellyllama <delaram.joo@gmail.com>
2021-07-22 15:20:18 +0100

    Multiple choice quiz fragment created and layout updated

---
+++ app/src/main/java/com/del/studyenglish1/MultipleChoiceQuiz.java
@@ -0,0 +1,17 @@
+package com.del.studyenglish1;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MultipleChoiceQuiz extends Fragment {
+
+    public MultipleChoiceQuiz() {
+        // Required empty public constructor
+    }
+
+}
