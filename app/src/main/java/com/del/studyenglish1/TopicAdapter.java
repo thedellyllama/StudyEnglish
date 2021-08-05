d39d9180f5eefb897c31a440623c3326b7a78015
thedellyllama <delaram.joo@gmail.com>
2021-08-05 15:10:33 +0100

    layouts updated

--- app/src/main/java/com/del/studyenglish1/TopicAdapter.java
+++ app/src/main/java/com/del/studyenglish1/TopicAdapter.java
@@ -6,7 +6,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -29,7 +28,7 @@ public class TopicAdapter extends RecyclerView.Adapter<TopicAdapter.TopicViewHol
         public TopicViewHolder(@NonNull View itemView) {
             super(itemView);
 
-            nameText = itemView.findViewById(R.id.button_topic_item);
+            nameText = itemView.findViewById(R.id.text_view_topic_item);
         }
     }
 
