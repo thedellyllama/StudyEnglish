51112ab8d6f24b6b953cb546110e1bdee5bc362d
thedellyllama <delaram.joo@gmail.com>
2021-08-10 14:46:09 +0100

    comments added

--- app/src/main/java/com/del/studyenglish1/Activity.java
+++ app/src/main/java/com/del/studyenglish1/Activity.java
@@ -10,20 +10,26 @@ public class Activity {
     public static final int ACT_NUM_3 = 3;
     public static final int ACT_NUM_4 = 4;
 
-
-    //private int id;
     private int topicId;
     private int activityNum;
     private String completed;
     private Date timeStamp;
 
+    /**empty Activity constructor**/
     public Activity(){}
 
+    /**Activity constructor
+     *
+     * @param topicId   the topicId referring to the _ID column of the Topics table
+     * @param activityNum   the number of the activity
+     * @param completed 'TRUE' if activity has been completed, 'FALSE' otherwise
+     */
     public Activity(int topicId, int activityNum, String completed) {
         this.topicId = topicId;
         this.activityNum = activityNum;
         this.completed = completed;
     }
+
     public int getTopicId() {
         return topicId;
     }
