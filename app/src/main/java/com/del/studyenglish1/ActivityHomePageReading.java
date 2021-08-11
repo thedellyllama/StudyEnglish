ce5570d0f210d7422907720114c45a79b717306c
thedellyllama <delaram.joo@gmail.com>
2021-08-11 16:35:29 +0100

    back button

--- app/src/main/java/com/del/studyenglish1/ActivityHomePageReading.java
+++ app/src/main/java/com/del/studyenglish1/ActivityHomePageReading.java
@@ -29,8 +29,8 @@ public class ActivityHomePageReading extends Fragment {
     private int activityNum;
 
     private TextView textViewTopic;
-    private TextView changeTopic;
-    private TextView toExplanation;
+    //private TextView changeTopic;
+    private ImageView backButton;
     private ImageView imageProgress;
     private Button activity1;
     private Button activity2;
@@ -84,8 +84,8 @@ public class ActivityHomePageReading extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        changeTopic = (TextView) view.findViewById(R.id.text_view_change_topic);
-        toExplanation = (TextView) view.findViewById(R.id.text_view_to_explanation);
+        //changeTopic = (TextView) view.findViewById(R.id.text_view_change_topic);
+        backButton = view.findViewById(R.id.button_back);
 
         updateButtonTexts();
 
@@ -105,33 +105,33 @@ public class ActivityHomePageReading extends Fragment {
                     openReadingQuiz(topic, type, level_name, activityNum);
                 }
         });
-
+/*
            changeTopic.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 toExplanationPage(topic, type, level_name);
             }
         });
-
-        toExplanation.setOnClickListener(new View.OnClickListener() {
+*/
+        backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                changeTopicPage(type, level, level_name);
+                openTopicHomePage(type, level, level_name);
             }
         });
     }
-    /**
+    /*
      * Opens new instance of the Select Topic Page with selected arguments
      * @param topic the selected topic name
      * @param type the selected topic type
      * @param level_name the selected level name
-     */
+
     public void toExplanationPage(String topic, String type, String level_name) {
         selectTopicPage = selectTopicPage.newInstance(type, level, level_name);
         FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
         fragmentTransaction.replace(R.id.nav_host_fragment, selectTopicPage);
         fragmentTransaction.commit();
-    }
+    }*/
 
     /**
      * Opens a new instance of Topic Home Page with selected arguments
@@ -139,7 +139,7 @@ public class ActivityHomePageReading extends Fragment {
      * @param level the selected level description
      * @param level_name the selected level name
      */
-    public void changeTopicPage(String type, String level, String level_name) {
+    public void openTopicHomePage(String type, String level, String level_name) {
         topicHomePage = topicHomePage.newInstance(topic, type, level, level_name);
         FragmentTransaction fragmentTransaction = getActivity().getSupportFragmentManager().beginTransaction();
         fragmentTransaction.replace(R.id.nav_host_fragment, topicHomePage);
